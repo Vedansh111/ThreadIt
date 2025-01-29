722dba7d6bd2e7d07ee4bf4a6d6e474b537abeaf
Yeezy2024 <vedansh.t@technmanconsulting.com>
2025-01-29 13:46:41 -0500

    Done till Infinie Scrolling

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,13 +1,17 @@
 "use client";
 
-import { FC, useCallback, useRef } from "react";
+import { FC, useCallback, useEffect, useRef, useState } from "react";
 import TextareaAutosize from "react-textarea-autosize";
-import { useForm } from "react-hook-form";
-import { PostCreationPayload, PostValidator } from "@/lib/validators/post";
+import { useForm, Controller } from "react-hook-form";
+import { PostCreationRequest, PostValidator } from "@/lib/validators/post";
 import { zodResolver } from "@hookform/resolvers/zod";
 import type EditorJS from "@editorjs/editorjs";
 import AWS from "aws-sdk";
 import { toast } from "@/hooks/use-toast";
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
+import { usePathname, useRouter } from "next/navigation";
+import "@/styles/editor.css";
 
 interface EditorProps {
   subredditId: string;
@@ -17,8 +21,9 @@ const Editor: FC<EditorProps> = ({ subredditId }) => {
   const {
     register,
     handleSubmit,
+    control,
     formState: { errors },
-  } = useForm<PostCreationPayload>({
+  } = useForm<PostCreationRequest>({
     resolver: zodResolver(PostValidator),
     defaultValues: {
       subredditId,
@@ -27,9 +32,15 @@ const Editor: FC<EditorProps> = ({ subredditId }) => {
     },
   });
 
+  const pathname = usePathname();
+  const router = useRouter();
   const ref = useRef<EditorJS>();
+  const [isMounted, setIsMounted] = useState<boolean>(false);
+  const _titleRef = useRef<HTMLTextAreaElement>(null);
 
   const initializeEditor = useCallback(async () => {
+    if (!isMounted || ref.current) return;
+
     const EditorJS = (await import("@editorjs/editorjs")).default;
     const Header = (await import("@editorjs/header")).default;
     const Embed = (await import("@editorjs/embed")).default;
@@ -40,90 +51,185 @@ const Editor: FC<EditorProps> = ({ subredditId }) => {
     const InlineCode = (await import("@editorjs/inline-code")).default;
     const ImageTool = (await import("@editorjs/image")).default;
 
-    if (!ref.current) {
-      const editor = new EditorJS({
-        holder: "editor",
-        onReady() {
-          ref.current = editor;
-        },
-        placeholder: "Type here to write your post...",
-        inlineToolbar: true,
-        data: { blocks: [] },
-        tools: {
-          header: Header,
-          linkTool: {
-            class: LinkTool,
-            config: {
-              endPoint: "/api/link",
-            },
+    ref.current = new EditorJS({
+      holder: "editor",
+      placeholder: "Type here to write your post...",
+      inlineToolbar: true,
+      tools: {
+        header: Header,
+        linkTool: {
+          class: LinkTool,
+          config: {
+            endPoint: "/api/link",
           },
-          image: {
-            class: ImageTool,
-            config: {
-              uploader: {
-                async uploadByFile(file: File) {
-                  const formData = new FormData();
-                  formData.append("image", file);
-
+        },
+        image: {
+          class: ImageTool,
+          config: {
+            uploader: {
+              async uploadByFile(file: File) {
+                try {
                   AWS.config.update({
-                    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-                    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-                    region: process.env.AWS_REGION,
+                    accessKeyId: process.env.NEXT_PUBLIC_AWS_ACCESS_KEY,
+                    secretAccessKey:
+                      process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY,
+                    region: process.env.NEXT_PUBLIC_AWS_REGION,
                   });
 
                   const s3 = new AWS.S3();
-
                   const params = {
-                    Bucket: process.env.S3_BUCKET as string,
-                    Key: file.name,
+                    Bucket: process.env.NEXT_PUBLIC_S3_BUCKET as string,
+                    Key: `uploads/${Date.now()}-${file.name}`,
                     Body: file,
                     ContentType: file.type,
                   };
 
-                  const upload = s3
-                    .upload(params)
-                    .on("httpUploadProgress", (evt) => {
-                      console.log(
-                        "Uploading " +
-                          parseInt(
-                            String((evt.loaded * 100) / (evt.total || 1))
-                          ) +
-                          "%"
-                      );
-                    })
-                    .promise();
+                  const upload = await s3.upload(params).promise();
 
-                  await upload
-                    .then(() => {
-                      return toast({
-                        title: "File Uploaded!",
-                        description: "Your file was uploaded successfully.",
-                      });
-                    })
-                    .catch(() => {
-                      return toast({
-                        title: "Failed to Upload File!",
-                        description: "There was an error uploading your file.",
-                        variant: "destructive",
-                      });
-                    });
-                },
+                  toast({
+                    title: "File Uploaded!",
+                    description: "Your file was uploaded successfully.",
+                  });
+
+                  return {
+                    success: 1,
+                    file: {
+                      url: upload.Location,
+                    },
+                  };
+                } catch (error) {
+                  toast({
+                    title: "Failed to Upload File!",
+                    description: "There was an error uploading your file.",
+                    variant: "destructive",
+                  });
+
+                  return { success: 0 };
+                }
               },
             },
           },
         },
-      });
+        list: List,
+        code: Code,
+        inlineCode: InlineCode,
+        table: Table,
+        embed: Embed,
+      },
+    });
+  }, [isMounted]);
+
+  useEffect(() => {
+    initializeEditor();
+    setTimeout(() => {
+      _titleRef.current?.focus();
+    }, 0);
+
+    return () => {
+      ref.current?.destroy();
+      ref.current = undefined;
+    };
+  }, [initializeEditor]);
+
+  useEffect(() => {
+    if (Object.keys(errors).length) {
+      for (const [_key, value] of Object.entries(errors)) {
+        console.log(value);
+        value;
+        toast({
+          title: "Something went wrong!",
+          description: (value as { message: string }).message,
+          variant: "destructive",
+        });
+      }
     }
+  }, [errors]);
+
+  useEffect(() => {
+    setIsMounted(true);
   }, []);
 
+  const { mutate: createPost } = useMutation({
+    mutationFn: async ({
+      title,
+      content,
+      subredditId,
+    }: PostCreationRequest) => {
+      const payload: PostCreationRequest = {
+        title,
+        content,
+        subredditId,
+      };
+
+      const { data } = await axios.post("/api/subreddit/post/create", payload);
+      return data;
+    },
+    onError: () => {
+      return toast({
+        title: "Failed to create post!",
+        description: "There was an error creating your post.",
+        variant: "destructive",
+      });
+    },
+    onSuccess: () => {
+      const newPathname = pathname.split("/").slice(0, -1).join("/");
+
+      router.push(newPathname);
+      router.refresh();
+
+      return toast({
+        title: "Post created successfully!",
+        description: "Your post has been created successfully.",
+      });
+    },
+  });
+
+  async function onSubmit(data: PostCreationRequest) {
+    const blocks = await ref.current?.save();
+
+    const payload: PostCreationRequest = {
+      title: data.title,
+      content: blocks,
+      subredditId,
+    };
+
+    createPost(payload);
+  }
+
+  const { ref: titleRef, ...rest } = register("title");
+
   return (
-    <div className="w-full p-4 bg-zinc-50 rounded-lg border border-zinc-200 ">
-      <form id="subreddit-post-form" className="w-fit" onSubmit={() => {}}>
+    <div className="w-full p-4 bg-zinc-50 rounded-lg border border-zinc-200">
+      <form
+        id="subreddit-post-form"
+        className="w-fit"
+        onSubmit={handleSubmit(onSubmit)}
+      >
         <div className="prose prose-stone dark:prose-invert">
-          <TextareaAutosize
-            placeholder="Title"
-            className="w-full resize-none appearance-none overflow-hidden bg-transparent text-5xl font-bold focus:outline-none"
+          <Controller
+            name="title"
+            control={control}
+            render={({ field }) => (
+              <TextareaAutosize
+                {...field}
+                placeholder="Title"
+                className="w-full resize-none appearance-none overflow-hidden bg-transparent text-5xl font-bold focus:outline-none"
+                ref={(e) => {
+                  field.ref(e);
+                  //@ts-ignore
+                  _titleRef.current = e;
+                }}
+              />
+            )}
           />
+          <div id="editor" className="min-h-[500px]" />
+          <p className="text-sm text-gray-500">
+            Use{" "}
+            <kbd className="rounded-md border bg-muted px-1 text-xs uppercase">
+              Tab
+            </kbd>{" "}
+            to open the command menu.
+          </p>
         </div>
       </form>
     </div>
--- src/lib/validators/post.ts
+++ src/lib/validators/post.ts
@@ -4,9 +4,9 @@ export const PostValidator = z.object({
   title: z
     .string()
     .min(3, { message: "Title must be longer than 3 characters" })
-    .max(128, { message: "Title must be at least 128 characters" }),
+    .max(128, { message: "Title must be at most 128 characters" }),
   content: z.any(),
   subredditId: z.string(),
 });
 
-export type PostCreationPayload = z.infer<typeof PostValidator>;
+export type PostCreationRequest = z.infer<typeof PostValidator>;
